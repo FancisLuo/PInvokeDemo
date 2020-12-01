7777543eb9becfbf6e3a6615bf05ab0a09befa70
FancisLuo <luojianyong6@live.com>
2020-12-01 21:37:59 +0800

    C#与C++互操作demo

---
+++ MyCSharpClient/DLLInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MyCSharpClient
+{
+    public class DLLInterface
+    {
+        private const string DllName = @"G:\TestSpace\CPPTest\PInvokeTest\MyConsoleApp\x64\Debug\CPPDemoDll.dll";
+
+        [DllImport(DllName, EntryPoint = "Add")]
+        private extern static int Add(int a, int b);
+
+        public static int DLL_Add(int a, int b)
+        {
+            return Add(a, b);
+        }
+    }
+}
---
+++ MyCSharpClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyCSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DLLInterface.DLL_Add(2, 4);
+            Console.WriteLine($"Add result = {result}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
